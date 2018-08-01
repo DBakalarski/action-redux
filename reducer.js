85325a7adc949c1744440c39d672a34015277a31
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-08-01 12:48:07 +0200

    Excercise 19.4

--- reducer.js
+++ reducer.js
@@ -1,37 +1,13 @@
-import {ADD_COMMENT} from './actions'
-import {EDIT_COMMENT} from './actions'
-import {REMOVE_COMMENT} from './actions'
-import {THUMB_UP_COMMENT} from './actions'
-import {THUMB_DOWN_COMMENT} from './actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
+import users from './users';
 
 const initialState = {
 	comments: [],
 	users: []
 };
 
-/*
-function reducer(state, action) {
-	if (!state) {
-		return initialState;
-	}
-	return state;
-}
-
-to samo tylko skrócone" */
-
-function reducer(state = initailState, action) {
-	switch(action.type) {
-		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [
-				{
-					id: action.id,
-					text: action.text,
-					votes: 0
-				}
-				, ...state]
-			})
-		default:
-			return state;
-	}
-}
+const app = combineReducers({
+    comments,
+    users
+});
